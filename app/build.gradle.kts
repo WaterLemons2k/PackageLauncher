98b99e1b12a62ecb6cda523258a6bb1cc7c934e2
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2024-10-30 13:59:50 +0800

    chore(app): exclude all unnecessary files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,13 +18,15 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
         }
     }
+    // exclude all unnecessary files
+    packaging.resources.excludes += "**"
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    kotlinOptions.jvmTarget = "1.8"
 }
